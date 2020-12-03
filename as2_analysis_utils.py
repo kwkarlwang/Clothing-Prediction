cb9902acadddbd94125ea0aaf016caf1c81a78cf
Karl Wang <kawang@ucsd.edu>
2020-12-02 23:42:06 -0800

    implment n-gram and setup fit prediction prototype

--- as2_analysis_utils.py
+++ as2_analysis_utils.py
@@ -10,6 +10,7 @@ import string
 import gzip
 from collections import defaultdict
 
+
 def parseData_line(path_to_file):
     """function to parse json file line by line, each line 
     of the file is a json structure
@@ -21,9 +22,11 @@ def parseData_line(path_to_file):
         dict: python dict that contain data from each line
     """
     for line in open(path_to_file):
+        line = line.replace("null", '"null"')
         yield ast.literal_eval(line)
 
-def tokenize_paragraph(p_str,remove_punc=True,n=1):
+
+def tokenize_paragraph(p_str, remove_punc=True, n=1):
     """Tokenize all words in a paragraph
 
     Args:
@@ -33,22 +36,39 @@ def tokenize_paragraph(p_str,remove_punc=True,n=1):
     Returns:
         list: list of all words in the paragraph
     """
-    assert isinstance(remove_punc,bool),"must be bool"
-    assert n == 1 or n == 2,"unigram or bigram"
+    assert isinstance(remove_punc, bool), "must be bool"
+    assert n >= 1, "n must be positive"
     p_str = p_str.lower()
 
     if remove_punc:
         p_str = [c for c in p_str if not c in string.punctuation]
     else:
-        p_str = [c if not c in string.punctuation else " "+c+" " for c in p_str]
+        p_str = [c if not c in string.punctuation else " " + c + " " for c in p_str]
 
-    p_str = ''.join(p_str)
+    p_str = "".join(p_str)
     p_list = p_str.strip().split()
 
-    if n==1:
+    if n == 1:
         return p_list
-    elif n==2:
-        return [bigram for bigram in zip(p_list[:-1],p_list[1:])]
+    elif n == 2:
+        return [bigram for bigram in zip(p_list[:-1], p_list[1:])]
+    else:
+        return n_gram(p_list, n)
+
+
+def n_gram(p_list, n=3):
+    """Create n-gram given a paragraph
+
+    Args:
+        p_list (list): paragraph splitted into list of words
+        n: n in n_gram
+    Returns:
+        list: list of n-gram from p_list
+    """
+    return [
+        tuple(p_list[j] for j in range(i, i + n)) for i in range(len(p_list) - n + 1)
+    ]
+
 
 def count_word_freq(docs_all):
     """Count number of times a word appears
@@ -59,7 +79,9 @@ def count_word_freq(docs_all):
     Returns:
         dict: ngram_freq, counter how many times a words has appeard
     """
-    assert isinstance(docs_all[0][0],str) or isinstance(docs_all[0][0],tuple),"input must be tokenized"
+    assert isinstance(docs_all[0][0], str) or isinstance(
+        docs_all[0][0], tuple
+    ), "input must be tokenized"
     ngram_freq = defaultdict(int)
 
     # create a frequency dict of all unique ngram
