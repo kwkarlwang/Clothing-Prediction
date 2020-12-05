dab4daa9aa92924c0e3085fdc03f523f634e6990
Charlie <457511811@qq.com>
2020-12-05 14:29:06 -0800

    add ordinal calssfier

--- as2_analysis_utils.py
+++ as2_analysis_utils.py
@@ -9,7 +9,7 @@ import numpy as np
 import string
 import gzip
 from collections import defaultdict
-
+from sklearn.base import clone
 
 def parseData_line(path_to_file):
     """function to parse json file line by line, each line 
@@ -174,3 +174,39 @@ def train_val_test_pipeline(model, data_all):
     test_cm = compute_confusion_matrix(data_all["test_y"], y_test_pred, normalized=True)
 
     return val_cm, test_cm
+
+class OrdinalClassifier():
+    """Ordinal Classifier based on any binary 
+    classifier. See https://towardsdatascience.com/simple-trick-to-train-an-ordinal-regression-with-any-classifier-6911183d2a3c
+    """
+    def __init__(self, clf):
+        self.clf = clf
+        self.clfs = {}
+    
+    def fit(self, X, y):
+        self.unique_class = np.sort(np.unique(y))
+        if self.unique_class.shape[0] > 2:
+            for i in range(self.unique_class.shape[0]-1):
+                # for each k - 1 ordinal value we fit a binary classification problem
+                binary_y = (y > self.unique_class[i]).astype(np.uint8)
+                clf = clone(self.clf)
+                clf.fit(X, binary_y)
+                self.clfs[i] = clf
+    
+    def predict_proba(self, X):
+        clfs_predict = {k:self.clfs[k].predict_proba(X) for k in self.clfs}
+        predicted = []
+        for i,y in enumerate(self.unique_class):
+            if i == 0:
+                # V1 = 1 - Pr(y > V1)
+                predicted.append(1 - clfs_predict[y][:,1])
+            elif y in clfs_predict:
+                # Vi = Pr(y > Vi-1) - Pr(y > Vi)
+                 predicted.append(clfs_predict[y-1][:,1] - clfs_predict[y][:,1])
+            else:
+                # Vk = Pr(y > Vk-1)
+                predicted.append(clfs_predict[y-1][:,1])
+        return np.vstack(predicted).T
+    
+    def predict(self, X):
+        return np.argmax(self.predict_proba(X), axis=1)
